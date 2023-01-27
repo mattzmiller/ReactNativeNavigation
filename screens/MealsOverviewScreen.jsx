1494be573c6964a0a18c93bbf2aab29fad0a8f1c
mzecic <matejzecic@gmail.com>
2023-01-27 09:19:15 -0500

    add navigation hooks for options on detail page

--- screens/MealsOverviewScreen.jsx
+++ screens/MealsOverviewScreen.jsx
@@ -1,27 +1,39 @@
-import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
+import { MEALS, CATEGORIES } from "../data/dummy-data";
 import { FlatList, StyleSheet, View } from "react-native";
 
 import MealItem from "../components/MealItem";
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((meal) => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
 
+  useLayoutEffect(
+    function () {
+      const categoryTitle = CATEGORIES.find((category) => {
+        return category.id === catId;
+      }).title;
+
+      navigation.setOptions({
+        title: categoryTitle,
+      });
+    },
+    [catId, navigation]
+  );
+
   function renderMealItem(itemData) {
-    const item = itemData.item
+    const item = itemData.item;
     const mealItemProps = {
-        title: item.title,
-        imageUrl: item.imageUrl,
-        affordability: item.affordability,
-        complexity: item.complexity,
-        duration: item.duration,
-    }
-    return (
-      <MealItem {...mealItemProps}/>
-    );
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
